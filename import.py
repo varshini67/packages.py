ec7c03b8584856c2627eee284cf9124137d9f8ad
varshini67 <157580242+varshini67@users.noreply.github.com>
2024-07-31 14:59:00 +0530

    Create import.py

---
+++ import.py
@@ -0,0 +1,11 @@
+from Robots import Car2
+Car2.Checkcar()
+car2.cleancar()
+def clencar():
+  c=int(input("please enter a price you can pay: ")
+        if(c>1000):
+            print("I am a robot Who cleans the car for",c)
+        else:
+            print("I am sorry price too low to clean the car ")
+def Checkcar():
+    print("I am a robot who checks the car and certifies")
